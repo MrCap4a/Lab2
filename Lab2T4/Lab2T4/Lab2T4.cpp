de79eacc48c5d42452fa1db66b42ea6a785682a1
MrCap4a <2212denis@gmail.com>
2024-03-18 18:56:37 +0300

    Completed task 4

---
+++ Lab2T4/Lab2T4/Lab2T4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ifstream readfile("in.txt");
+	ofstream odd("odd.txt");
+	ofstream even("even.txt");
+
+	string line = "";
+	bool isOdd = true;
+
+	while (getline(readfile, line)) {
+		if (isOdd) {
+			odd << line << endl;
+		}
+		else {
+			even << line << endl;
+		}
+		isOdd = !isOdd;
+	}
+	readfile.close();
+	odd.close();
+	even.close();
+
+	cout << "Completed!";
+}
