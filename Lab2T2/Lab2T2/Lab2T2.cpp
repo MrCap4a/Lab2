b349a851a0afc1dc10ffe06f626a1aa92853c335
MrCap4a <2212denis@gmail.com>
2024-03-18 18:27:37 +0300

    Completed task 2

---
+++ Lab2T2/Lab2T2/Lab2T2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+//Дано 2 файла вещественных чисел а1.txt и а2.txt. Найти сумму положительных элементов в двух файлов.
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    ifstream fileA("a1.txt");
+    ifstream fileB("a2.txt");
+
+    string line = "";
+    int sum = 0;
+
+    while (getline(fileA, line)) {
+        cout << line << endl;
+        int num = stoi(line);
+        if (num > 0) {
+            sum += num;
+        }
+    }
+    cout << endl;
+
+    while (getline(fileB, line)) {
+        cout << line << endl;
+        int num = stoi(line);
+        if (num > 0) {
+            sum += num;
+        }
+    }
+    cout << endl;
+
+    cout << "Сумма положительных чисел: " << sum;
+}
